c2fc8f07db5618c7df441d8e4fe66389c2f0ac4d
Himalay <contact@himalay.com.np>
2019-08-16 07:47:00 +0545

    Add Ionic 4 React project

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+
+const Home: React.FunctionComponent = () => {
+  return (
+    <>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Khabar</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        Taja Khabar!
+      </IonContent>
+    </>
+  );
+};
+
+export default Home;
