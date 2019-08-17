8217c75697415d9fe08b9b391690c4fe86ce3bf1
Himalay <contact@himalay.com.np>
2019-08-17 13:23:22 +0545

    Add single feed fetch

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,40 +1,50 @@
 import { setLoading, setState } from '../state'
 
-const sources = {
-  hackernews: 'hackerNews',
-  github: 'github',
-  echojs: 'echojs',
-  devto: 'devto',
-  medium: 'medium',
-  reddit: 'redditprogramming',
-  producthunt: 'productHunt',
-}
+const sources = [
+  ['hackernews', 'hackerNews'],
+  ['github', 'github'],
+  ['echojs', 'echojs'],
+  ['devto', 'devto'],
+  ['medium', 'medium'],
+  ['reddit', 'redditprogramming'],
+  ['producthunt', 'productHunt'],
+]
 
 const baseURL =
   'https://cors-anywhere.herokuapp.com/cdnapi.pnd.gs/v2/feeds?limit=20&page=1&sort=popular&sources='
 const options = { headers: { Origin: 'https://khabar.github.io/' } }
 
-const fetchData = async () => {
+const fetchAndSave = async (key: string, source: string) => {
+  const data = await (await fetch(
+    baseURL + source,
+    options,
+  )).json()
+
+  setState(key, data)
+}
+
+
+const fetchData = async (sourceKey?: string) => {
   try {
-    setLoading(true)
-    await Promise.all(
-      Object.keys(sources).map(
-        (key: string, i: number) =>
-          new Promise(async (resolve, reject) => {
-            try {
-              const data = await (await fetch(
-                baseURL + Object.values(sources)[i],
-                options,
-              )).json()
-  
-              setState(key, data)
-              resolve()
-            } catch (error) {
-              reject(error)
-            }
-          }),
-      ),
-    )
+    if (sourceKey) {
+        const source = sources.find(([k]) => k === sourceKey)
+        if (source) await fetchAndSave(sourceKey, source[1])
+    } else {
+      setLoading(true)
+      await Promise.all(
+        sources.map(
+          ([key, source]) =>
+            new Promise(async (resolve, reject) => {
+              try {
+                await fetchAndSave(key, source)
+                resolve()
+              } catch (error) {
+                reject(error)
+              }
+            }),
+        ),
+      )
+    }
   } catch (error) {
     console.error(error)
   } finally {
