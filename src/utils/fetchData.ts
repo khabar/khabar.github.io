2f85a46f34eb76c7c51b5216d26a19874fb44617
Himalay <contact@himalay.com.np>
2019-12-14 09:20:18 +0545

    Fix data source

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,22 +1,20 @@
 import { setLoading, setState } from '../state'
 
 const sources = [
-  ['hackernews', 'hackerNews'],
-  ['github', 'github'],
-  ['echojs', 'echojs'],
-  ['devto', 'devto'],
-  ['medium', 'medium'],
-  ['reddit', 'reddit'],
-  ['growthhackers', 'growthHackers'],
-  ['datatau', 'datatau'],
-  ['redditprogramming', 'redditprogramming'],
-  ['producthunt', 'productHunt'],
+  ['hackernews', '5718e53e7a84fb1901e05971'],
+  ['github', '5718e53e7a84fb1901e059cc'],
+  ['echojs', '5718e53e7a84fb1901e0592e'],
+  ['devto', '5bbb1af8af62ff6841b4b26e'],
+  ['medium', '5718e53e7a84fb1901e05929'],
+  ['reddit', '5b910ac49642a1e1c6928ab0'],
+  ['growthhackers', '5718e53d7a84fb1901e0590f'],
+  ['datatau', '5718e53e7a84fb1901e0592f'],
+  ['redditprogramming', '59be493f3532e00343101d80'],
+  ['producthunt', '5718e53e7a84fb1901e059c7'],
 ]
-
 const baseURL =
-  'https://cors-anywhere.herokuapp.com/cdnapi.pnd.gs/v2/feeds?limit=20&page=1&sort=popular&sources='
+  'https://cors-anywhere.herokuapp.com/api.usepanda.com/v4/articles?limit=30&page=1&sort=popular&feeds='
 const options = { headers: { Origin: 'https://khabar.github.io/' } }
-
 const fetchAndSave = async (key: string, source: string) => {
   const data = (await (await fetch(baseURL + source, options)).json()).filter(
     (x: any) => x.source.likesCount === undefined || x.source.likesCount,
