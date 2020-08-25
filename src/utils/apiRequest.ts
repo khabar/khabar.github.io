14c882e974cb6de6999d1e4e43abb55b38562cd8
Himlay <contact@himalay.com.np>
2020-08-25 06:36:53 +0545

    Use cors-anywhere proxy

--- src/utils/apiRequest.ts
+++ src/utils/apiRequest.ts
@@ -1,17 +1,22 @@
 import { setGlobalStatePersistent } from '../state'
 
-const API_BASEURL = 'https://api.usepanda.com'
+const API_BASEURL = 'https://cors-anywhere.herokuapp.com/https://api.usepanda.com'
+const requiredOptions = { headers: { Origin: 'https://khabar.github.io/' } }
 
 const apiRequest = async (path: string, options?: RequestInit) => {
   let status = 500
+  const fetchOptions = options
+    ? { ...options, headers: { ...(options.headers || {}), ...requiredOptions.headers } }
+    : requiredOptions
+
   try {
-    status = (await fetch(API_BASEURL)).status
+    status = (await fetch(API_BASEURL, fetchOptions)).status
   } catch {
     //
   }
   if (status === 200) {
     setGlobalStatePersistent('isOffline', false)
-    const res = await fetch(API_BASEURL + path, options)
+    const res = await fetch(API_BASEURL + path, fetchOptions)
     if (res.ok) return res.json()
   } else {
     setGlobalStatePersistent('isOffline', true)
