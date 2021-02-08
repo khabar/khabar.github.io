5cc26ad5555f0d8848f4fca4c46e51234c371bce
himalay <himalay@users.noreply.github.com>
2021-02-08 03:28:05 +0000

    deploy: cb68a419f4da4b3f97fcb287228f9d329554a589

---
+++ static/js/53.0dc7183f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpKhabar=this.webpackJsonpKhabar||[]).push([[53],{290:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e(11),i=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.h)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var r=i.closest("ion-content");r&&r.componentOnReady().then((function(){Object(o.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.0dc7183f.chunk.js.map
